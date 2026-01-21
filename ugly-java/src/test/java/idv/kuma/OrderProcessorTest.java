dbf23a1bd322d50e1ffe19e19aca71ef19d2d444
Yu-Song Syu <kuma@cloud-latitude.com>
2026-01-21 17:46:34 +0800

    hard to test

---
+++ ugly-java/src/test/java/idv/kuma/OrderProcessorTest.java
@@ -0,0 +1,27 @@
+package idv.kuma;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+import java.util.Map;
+
+class OrderProcessorTest {
+
+    @Test
+    void no_discounts() {
+
+        OrderProcessor sut = new OrderProcessor();
+
+        List<Map<String, Object>> order = List.of(
+                Map.of("p", 100D, "q", 1),
+                Map.of("p", 20D, "q", 2)
+        );
+
+        sut.process(order, "NORMAL", false);
+
+        // 不是我不測，靠北你寫這麼爛的 code 是要給鬼測嗎？
+        Assertions.assertThat( ? ? ?).isEqualTo(140D);
+
+    }
+}
